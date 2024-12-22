cf75a588029d369afd9104079416c39a5b7476d4
SIA <152020732+jyun-KIM@users.noreply.github.com>
2024-12-22 23:05:14 +0900

    Update 크로아티아 알파벳.py

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,18 +1,17 @@
-croba = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+croba = ['c=', 'c-', 'z=', 'd-', 'lj', 'nj', 's=', 'dz=']
 word = input()
 answer = 0
 i = 0
 
-while i < len(word):
-    found = False
-    for croba_alpha in croba:
-        if word[i:i+len(croba_alpha)] == croba_alpha:
-            answer += 1
-            i += len(croba_alpha)
-            found = True
+while i<len(word):
+    flag=0
+    for alpha in croba:
+        if word[i:i+len(alpha)]==alpha:
+            answer+=1
+            flag=1
+            i+=len(alpha)
             break
-    if not found:
-        answer += 1
-        i += 1
-
+    if flag==0:
+        answer+=1
+        i+=1
 print(answer)
