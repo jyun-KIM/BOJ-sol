2d12c3053fd4fed4287c2ae257362273e96afcdc
SIA <152020732+jyun-KIM@users.noreply.github.com>
2025-01-06 14:52:04 +0900

    Update 괄호의 값.py

--- 백준/Gold/2504. 괄호의 값/괄호의 값.py
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -7,47 +7,33 @@ for i in range(len(arr)):
   if arr[i] =='(':
     stack.append(arr[i])
     tmp*=2
-    # print(tmp)
   elif arr[i] =='[':
     stack.append(arr[i])
     tmp*=3
-    # print(tmp)
   elif arr[i]==')':
     if len(stack)==0:
       answer=0
       break
     j=stack.pop()
-    # if len(stack)==0:
-    #   answer+=tmp
-    #   tmp=1 #확인 
     if arr[i-1]=='(':
       answer+=tmp
-      # print("answer1:", answer)
     tmp//=2
-    # print(tmp)
     if j!='(':
       answer=0
       tmp=1
       break
-    # tmp//=2
   elif arr[i]==']':
     if len(stack)==0:
       answer=0
       break
     j=stack.pop()
-    # if len(stack)==0:
-    #   answer+=tmp
-    #   tmp=1
     if arr[i-1]=='[':
       answer+=tmp
-      # print("answer2: ",answer)
     tmp//=3
-    # print(tmp)
     if j!='[':
       answer=0
       tmp=1
       break
-    # tmp//=3
   
 if len(stack)!=0:
   answer=0
