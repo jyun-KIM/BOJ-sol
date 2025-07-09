18f93133f742f6ca834f4be7ec0ad168b1b89d54
JYUN 김지현 <152020732+jyun-KIM@users.noreply.github.com>
2025-07-09 10:49:46 +0900

    [Silver V] Title: K번째 수, Time: 1908 ms, Memory: 376556 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+        System.out.println(A[k - 1]);
+    }
+}
