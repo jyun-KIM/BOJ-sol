28af9998df1dfaf632d1b5d25e9f4be6fede5a3e
김지현 jihyun Kim <152020732+jyun-KIM@users.noreply.github.com>
2026-02-20 01:23:43 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 1011.93 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -0,0 +1,11 @@
+import math
+def solution(r1, r2):
+    answer = 0
+    for x in range(1,r2+1):
+        ymax=math.floor(math.sqrt(r2**2-x**2))
+        if x < r1:
+            ymin = math.ceil(math.sqrt(r1**2 - x**2))
+        else:
+            ymin = 0
+        answer+=(ymax-ymin)+1
+    return answer*4
