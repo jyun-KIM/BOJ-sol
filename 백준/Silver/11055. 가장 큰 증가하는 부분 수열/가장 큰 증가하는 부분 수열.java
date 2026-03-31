94f9a12a83eb4ffe8f279192b0f91496abba43b6
김지현 jihyun Kim <152020732+jyun-KIM@users.noreply.github.com>
2026-03-31 17:06:11 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 128 ms, Memory: 14644 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+          arr[i]=Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        int answer=0;
+        //dp[i]는 i를 포함하는 부분수열 합 최댓값 
+        for(int i=0;i<n;i++){
+            dp[i]=arr[i];
+            for(int j=0;j<i;j++){
+                if(arr[j]<arr[i]){
+                    dp[i]=Math.max(dp[i],dp[j]+arr[i]);
+                    // System.out.printf("dp[i]: %d. arr[j]: %d\n", dp[i],arr[j]);
+                }
+            }
+            answer=Math.max(answer, dp[i]);
+        }
+
+        System.out.println(answer);
+    }
+}
