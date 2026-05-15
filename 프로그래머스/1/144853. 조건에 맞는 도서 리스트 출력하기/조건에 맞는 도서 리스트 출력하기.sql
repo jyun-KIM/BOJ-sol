559c01299adc5e3dc78ee93115f0248d69d511e1
김지현 jihyun Kim <152020732+jyun-KIM@users.noreply.github.com>
2026-05-15 09:25:53 +0900

    [level 1] Title: 조건에 맞는 도서 리스트 출력하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/144853. 조건에 맞는 도서 리스트 출력하기/조건에 맞는 도서 리스트 출력하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT 
+    BOOK_ID, 
+    DATE_FORMAT(PUBLISHED_DATE, '%Y-%m-%d') as PUBLISHED_DATE
+from book
+where CATEGORY='인문'
+    and year(PUBLISHED_DATE)=2021
+
+order by PUBLISHED_DATE asc;
